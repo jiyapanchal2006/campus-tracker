8f125df3c1bc23b51c60208c0ac70a0da3b43348
jiyapanchal2006 <jiyapanchal2006@gmail.com>
2026-04-20 19:02:55 +0530

    fixed frontend api url

--- campus-tracker/frontend/src/api.js
+++ campus-tracker/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({ 
- baseURL: 'https://your-backend.onrender.com/api' 
+  baseURL: 'https://your-backend.onrender.com/api' 
 });
 
 API.interceptors.request.use((config) => {
